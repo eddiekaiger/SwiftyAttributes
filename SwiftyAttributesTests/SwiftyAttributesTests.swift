9a66c1d863f742fb7091b42c1f5e217600d5e227
eddie-kaiger <eddiekaiger@gmail.com>
2016-10-15 02:06:08 -0700

    Add tests for WritingDirection.

--- SwiftyAttributesTests/SwiftyAttributesTests.swift
+++ SwiftyAttributesTests/SwiftyAttributesTests.swift
@@ -122,13 +122,22 @@ class SwiftyAttributesTests: XCTestCase {
     }
 
     func testAttribute_writingDirection() {
-        let subject = "Hello".withWritingDirections([.leftToRightOverride, .rightToLeftEmbedding])
+        let subject = "Hello".withWritingDirections([.leftToRightOverride, .rightToLeftEmbedding, .leftToRightOverride, .rightToLeftEmbedding])
         let directions = [
+            NSWritingDirection.leftToRight.rawValue | NSWritingDirectionFormatType.override.rawValue,
+            NSWritingDirection.rightToLeft.rawValue | NSWritingDirectionFormatType.embedding.rawValue,
             NSWritingDirection.leftToRight.rawValue | NSWritingDirectionFormatType.override.rawValue,
             NSWritingDirection.rightToLeft.rawValue | NSWritingDirectionFormatType.embedding.rawValue
         ]
         let expected = NSAttributedString(string: "Hello", attributes: [NSWritingDirectionAttributeName: directions])
         XCTAssertEqual(subject, expected)
+    }
+
+    func testWritingDirectionRawValue() {
+        XCTAssertEqual(WritingDirection(rawValue: (NSWritingDirection.leftToRight.rawValue | NSWritingDirectionFormatType.override.rawValue)), .leftToRightOverride)
+        XCTAssertEqual(WritingDirection(rawValue: (NSWritingDirection.rightToLeft.rawValue | NSWritingDirectionFormatType.override.rawValue)), .rightToLeftOverride)
+        XCTAssertEqual(WritingDirection(rawValue: (NSWritingDirection.leftToRight.rawValue | NSWritingDirectionFormatType.embedding.rawValue)), .leftToRightEmbedding)
+        XCTAssertEqual(WritingDirection(rawValue: (NSWritingDirection.rightToLeft.rawValue | NSWritingDirectionFormatType.embedding.rawValue)), .rightToLeftEmbedding)
     }
 
     func testOverloadedAdditionOperator() {
