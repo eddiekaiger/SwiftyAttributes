fe1f476fa4ff2ff31246285f021c799af2d5b998
eddie-kaiger <eddiekaiger@gmail.com>
2016-11-12 23:10:49 -0800

    Remove platform-specific targets in favor of a unified framework target and testing target for all platforms.

---
+++ SwiftyAttributes/SwiftyAttributes.h
@@ -0,0 +1,12 @@
+//
+//  SwiftyAttributes.h
+//  SwiftyAttributes
+//
+//  Created by Eddie Kaiger on 11/9/16.
+//  Copyright © 2016 Eddie Kaiger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double SwiftyAttributesVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftyAttributesVersionString[];
