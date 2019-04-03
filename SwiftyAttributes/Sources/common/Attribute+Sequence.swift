5ed476dfac21cc7416cbdc80ed99f48badaf09b7
Eddie Kaiger <eddie.kaiger@instacart.com>
2019-04-03 02:40:09 -0700

    Upgrade to Swift 5 and drop support for everything below Swift 4.2.

--- SwiftyAttributes/Sources/common/Attribute+Sequence.swift
+++ SwiftyAttributes/Sources/common/Attribute+Sequence.swift
@@ -9,41 +9,21 @@
 /**
  An extension on dictionaries that allows us to convert a Foundation-based dictionary of attributes to an array of `Attribute`s.
  */
-#if swift(>=4.0)
-    extension Dictionary where Key == AttributeName {
 
-        /// Returns an array of `Attribute`s converted from the dictionary of attributes. Use this whenever you want to convert [NSAttributeStringKey: Any] to [Attribute].
-        public var swiftyAttributes: [Attribute] {
-            return map(Attribute.init)
-        }
+extension Dictionary where Key == NSAttributedString.Key {
 
+    /// Returns an array of `Attribute`s converted from the dictionary of attributes. Use this whenever you want to convert [NSAttributeStringKey: Any] to [Attribute].
+    public var swiftyAttributes: [Attribute] {
+        return map(Attribute.init)
     }
-#else
-/**
- A Sequence with an iterator of (String, Any) is equivalent to [String: Any]
- This is a simple syntactic workaround since we can't write "extension Dictionary where Key == String". Thanks Swift :)
- */
-    extension Sequence where Iterator.Element == (key: String, value: Any) {
 
-        /// Returns an array of `Attribute`s converted from the dictionary of attributes. Use this whenever you want to convert [String: Any] to [Attribute].
-        public var swiftyAttributes: [Attribute] {
-            return flatMap { name, value in
-                if let attrName = AttributeName(rawValue: name) {
-                    return Attribute(name: attrName, foundationValue: value)
-                } else {
-                    return nil
-                }
-            }
-        }
-
-    }
-#endif
+}
 
 extension Sequence where Iterator.Element == Attribute {
 
     /// Returns the attribute dictionary required by Foundation's API for attributed strings. Use this whenever you need to convert [Attribute] to [String: Any].
-    public var foundationAttributes: [StringKey: Any] {
-        return reduce([StringKey: Any]()) { dictionary, attribute in
+    public var foundationAttributes: [NSAttributedString.Key: Any] {
+        return reduce([NSAttributedString.Key: Any]()) { dictionary, attribute in
             var dict = dictionary
             dict[attribute.keyName] = attribute.foundationValue
             return dict
